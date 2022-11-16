d950b0c79f08d2f44341142db87f4dcbe35d7e5b
Dairon <dairon10_@hotmail.com>
2022-11-16 07:47:16 -0300

    Second Commit

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,12 +1,14 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
+    @Qualifier("bubble")
     private SortAlgorithm sortAlgorithm;
 
     public int binaraySearch(int[] numbers, int numberToSearchFor){
